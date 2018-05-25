5aa218482ade18c09f9151588aff889685a87931
Dmytro Feshchenko <dimafe2000@gmail.com>
2018-05-25 15:59:06 +0300

    Added blynk-library example

--- server/web/assets/js/test.js
+++ server/web/assets/js/test.js
@@ -1,20 +1,18 @@
 var Blynk = require('blynk-library');
 
-var AUTH = '03f1323b6a5548b288c34c65a0ddca5d';
+var AUTH = 'a1436b98817c426ea91740829e164a3f';
 
-var blynk = new Blynk.Blynk(AUTH, options = {
-    connector : new Blynk.TcpClient( options = { addr:"gps-tracker.ml", port:8441 } )
-});
+var blynk = new Blynk.Blynk(AUTH,);
 
 var v1 = new blynk.VirtualPin(1);
-var v9 = new blynk.VirtualPin(9);
+var v12 = new blynk.VirtualPin(12);
 
 v1.on('write', function(param) {
     console.log('V1:', param);
 });
 
-v9.on('read', function() {
-    v9.write(new Date().getSeconds());
+v12.on('read', function() {
+    v12.write(new Date().getSeconds());
 });
 
 blynk.on('connect', function() { console.log("Blynk ready."); });
